a2755729e94c07e2e9ff7d65bc537eedd0405bba
phucnh204 <137475114+phucnh204@users.noreply.github.com>
2025-01-21 22:36:37 +0700

    Merge tash star into group-04

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import CalendarInput from './components/CalendarInput';
+import Star from './components/Start';
+import SubjectButton from './components/SubjectButton';
 
 const App = () => {
-  return <div className="p-4"></div>;
+  return (
+    <div className="p-4">
+      <Star isSelected />
+      <SubjectButton title="Nộp ngay" backgroundColor="blue" textColor="white" />
+      <SubjectButton title="Subject md" backgroundColor="blue" textColor="white" size="medium" />
+      <SubjectButton title="Subject large" backgroundColor="blue" textColor="white" size="large" iconPosition="right" iconName="XCircleBlue" />
+      <SubjectButton title="Subject medium" backgroundColor="blue" textColor="white" size="medium" iconPosition="right" iconName="XCircleBlue" />
+
+      <CalendarInput />
+    </div>
+  );
 };
 
 export default App;
