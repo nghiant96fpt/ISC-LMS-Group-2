03425975ac75d0b334db2f4ff1a453410f320ea3
phucnh204 <137475114+phucnh204@users.noreply.github.com>
2025-01-21 23:03:52 +0700

    Update App.tsx xóa nội dung file tấn merge from address-stack

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,6 @@ function App() {
     </div>
   );
 } 
-import CalendarInput from './components/CalendarInput';
-import Star from './components/Start';
-import SubjectButton from './components/SubjectButton';
 
-const App = () => {
-  return <div className="p-4"></div>;
-}; 
 
 export default App;
