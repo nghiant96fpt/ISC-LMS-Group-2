06c4e6cd1deabe9e7bd83f6a60e6723ce476e41a
phucnh204 <137475114+phucnh204@users.noreply.github.com>
2025-01-21 22:37:16 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,7 @@ import Star from './components/Start';
 import SubjectButton from './components/SubjectButton';
 
 const App = () => {
-  return (
-    <div className="p-4">
-      <Star isSelected />
-      <SubjectButton title="Nộp ngay" backgroundColor="blue" textColor="white" />
-      <SubjectButton title="Subject md" backgroundColor="blue" textColor="white" size="medium" />
-      <SubjectButton title="Subject large" backgroundColor="blue" textColor="white" size="large" iconPosition="right" iconName="XCircleBlue" />
-      <SubjectButton title="Subject medium" backgroundColor="blue" textColor="white" size="medium" iconPosition="right" iconName="XCircleBlue" />
-
-      <CalendarInput />
-    </div>
-  );
+  return <div className="p-4"></div>;
 };
 
 export default App;
