bdb3888e2bf39d1eaeae696f8c9f03bc07da200f
tanbvpc05190 <buivantan29082003@gmail.com>
2025-01-21 22:49:12 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-
-import './App.css';
-// import CalendarInput from './components/CalendarInput';
 import AddressList from './components/AddressUrlStack/Index';
 
 function App() {
   return (
     <div className="App">
-      <AddressList addressList={['Sample 1', 'Sample 2']} />
-      <AddressList addressList={['Sample 1', 'Sample 2', 'Sample 3', 'Simple 4']} />
+      <AddressList type={false} addressList={['Sample 1', 'Sample 2', 'Sample 3', 'Simple 4']} />
       <AddressList type={true} addressList={['Sample 1', 'Sample 2', 'Sample 3', 'Simple 4', 'Simple 5']} />
       <AddressList type={true} addressList={['Sample 1', 'Sample 2', 'Sample 3', 'Simple 4', 'Simple 5', 'Simple 6']} />
     </div>
