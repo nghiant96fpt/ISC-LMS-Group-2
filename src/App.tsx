77d4a70c8afbe714c410fe031eae9ac072a8da7d
phucnh204 <137475114+phucnh204@users.noreply.github.com>
2025-01-21 23:06:35 +0700

    merge code into group-04 from address-stack

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
-import React from 'react'; 
+import React from 'react';
 import AddressList from './components/AddressUrlStack/Index';
 
 function App() {
-  return (
-    <div className="App">
-      <AddressList type={false} addressList={['Sample 1', 'Sample 2', 'Sample 3', 'Simple 4']} />
-      <AddressList type={true} addressList={['Sample 1', 'Sample 2', 'Sample 3', 'Simple 4', 'Simple 5']} />
-      <AddressList type={true} addressList={['Sample 1', 'Sample 2', 'Sample 3', 'Simple 4', 'Simple 5', 'Simple 6']} />
-    </div>
-  );
-} 
-
+  return <div className="App"></div>;
+}
 
 export default App;
