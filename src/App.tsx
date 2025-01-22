0fd3363b2d5184130def5d8613dccdeded855796
doandvpc06922 <150895608+doandvpc06922@users.noreply.github.com>
2025-01-22 16:20:44 +0700

    commit/FrameTime/App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
 import React from 'react';
 import AddressList from './components/AddressUrlStack/Index';
-import FrameTime from './components/FrameTime/index';
 
 function App() {
-  return (
-    <>
-      <div className="App">
-        <FrameTime></FrameTime>
-      </div>
-    </>
-  );
+  return <div className="App"></div>;
 }
-
 
 export default App;
