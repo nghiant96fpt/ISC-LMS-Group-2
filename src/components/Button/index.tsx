3556bfa04a62acf5053ac7e1d44b0d1af3488574
tranvanchilinh1003 <chilinh140220@gmail.com>
2025-01-22 16:18:54 +0700

    Nhóm3-Component-Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,1 +1,32 @@
-export {}
+import React from 'react';
+import './style.css';
+import { ButtonProps } from './type';
+
+const Button: React.FC<ButtonProps> = ({
+    className: className = '',
+    disabled = false,
+    icon,
+    children,
+    onClick,
+    width,
+    height,
+    size = 'big',
+    type = 'button',
+    style,
+}) => {
+    const buttonClass = `button ${className} ${size}  ${disabled ? 'disabled' : ''}`;
+
+    return (
+        <button
+            className={buttonClass}
+            onClick={onClick}
+            style={{ ...style, width, height }}
+            type={type}
+        >
+            {icon && <span className="icon">{icon}</span>}
+            {children}
+        </button>
+    );
+};
+
+export default Button;
--- src/components/Button/type.tsx
+++ src/components/Button/type.tsx
@@ -1,1 +1,14 @@
-export {}
+import React from "react";
+
+export interface ButtonProps {
+    className?: 'primary' | 'secondary' | 'outline-primary' | 'outline-secondary';
+    icon?: React.ReactNode;
+    children: React.ReactNode;
+    onClick?: () => void;
+    width?: string;
+    height?: string;
+    disabled?: boolean;
+    size?: 'big' | 'mini';
+    type?: 'button' | 'submit' | 'reset',
+    style?: React.CSSProperties;
+}
