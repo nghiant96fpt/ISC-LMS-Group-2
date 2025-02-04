d71796d85abed46aeb52a00708ce3c46070deff2
phucnh204 <137475114+phucnh204@users.noreply.github.com>
2025-02-04 23:48:04 +0700

    Chỉnh cmt trong component Start

--- src/components/Start/type.tsx
+++ src/components/Start/type.tsx
@@ -1,19 +1,13 @@
 /**
  * Props cho component Star
- * 
- * @property {boolean} selected - Chỉ định ngôi sao có được chọn hay không.
- * @property {Function} toggleSelect - Hàm callback để thay đổi trạng thái chọn của ngôi sao.
+ *
+ * @property {boolean} selected
+ * @property {Function} toggleSelect
  */
 export interface StarProps {
-  /** 
-   * Trạng thái cho biết ngôi sao có được chọn hay không. 
-   * Giá trị này sẽ xác định xem ngôi sao có hình ảnh được chọn hay không.
-   */
+  /** Trạng thái của ngôi sao (đã chọn hoặc chưa chọn). */
   selected: boolean;
 
-  /** 
-   * Hàm callback để thay đổi trạng thái chọn của ngôi sao. 
-   * Hàm này sẽ được gọi khi người dùng nhấp vào ngôi sao.
-   */
+  /** Hàm xử lý chọn/bỏ chọn ngôi sao. */
   toggleSelect: () => void;
 }
