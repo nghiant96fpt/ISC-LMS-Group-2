26f58c5c38ecdab5c21b4ece7ad9464723b37d78
thuysvy <150228593+thuysvy@users.noreply.github.com>
2025-02-04 20:59:11 +0700

    fix Tag theo reviews

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 function App() {
   const [activeIndex, setActiveIndex] = useState<number | null>(0);
 
-  const handleTagClick = (index: number) => {
+  const handleTagClick = (index: number) => () => {
     setActiveIndex(index === activeIndex ? null : index);
   };
 
@@ -14,7 +14,7 @@ function App() {
   return (
     <>
       {tags.map((text, index) => (
-        <TagLayout key={index} text={text} isActive={activeIndex === index} onClick={() => handleTagClick(index)} />
+        <TagLayout key={index} text={text} isActive={activeIndex === index} onClick={handleTagClick(index)} />
       ))}
     </>
   );
