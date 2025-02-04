f6009fb89b4bb8c4baff5160cc4db464bdb765a3
thuanwbit <117891819+thuann17@users.noreply.github.com>
2025-02-04 14:13:02 -0800

    Fix code Selected và toggleSelect từ bên ngoài truyền vào

--- src/components/Start/type.tsx
+++ src/components/Start/type.tsx
@@ -1,3 +1,19 @@
+/**
+ * Props cho component Star
+ * 
+ * @property {boolean} selected - Chỉ định ngôi sao có được chọn hay không.
+ * @property {Function} toggleSelect - Hàm callback để thay đổi trạng thái chọn của ngôi sao.
+ */
 export interface StarProps {
-  isSelected: boolean;
+  /** 
+   * Trạng thái cho biết ngôi sao có được chọn hay không. 
+   * Giá trị này sẽ xác định xem ngôi sao có hình ảnh được chọn hay không.
+   */
+  selected: boolean;
+
+  /** 
+   * Hàm callback để thay đổi trạng thái chọn của ngôi sao. 
+   * Hàm này sẽ được gọi khi người dùng nhấp vào ngôi sao.
+   */
+  toggleSelect: () => void;
 }
