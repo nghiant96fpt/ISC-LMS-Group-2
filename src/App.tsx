1af3dc549ba6fcf81a0b7db22c73beedf47d58aa
phucnh204 <137475114+phucnh204@users.noreply.github.com>
2025-02-04 22:06:04 +0700

    Xóa 4 dong code không cần thiết
    file App.tsx
         <div className="p-4 ">
            <h1>Chọn ngày</h1>
            <CalendarInput />
          </div>

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
 
 import './App.css';
-import CalendarInput from './components/CalendarInput';
 
 function App() {
-  return (
-    <div className="App">
-      <div className="p-4 ">
-        <h1>Chọn ngày</h1>
-        <CalendarInput />
-      </div>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
