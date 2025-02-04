60a3589c312a831e18adf5a046da34e51c9e7eb2
phucnh204 <137475114+phucnh204@users.noreply.github.com>
2025-02-04 23:17:45 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
-
+import CalendarInput from './components/CalendarInput';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <CalendarInput />
+    </div>
+  );
 }
 
 export default App;
