aba916bc61b5565d74f1d539bdf862dabca16b61
tranvanchilinh1003 <chilinh140220@gmail.com>
2025-02-05 21:31:49 +0700

    fix code theo feedback, comment interface

--- src/components/Button/type.tsx
+++ src/components/Button/type.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-export interface ButtonProps {
-    className?: 'primary' | 'secondary' | 'outline-primary' | 'outline-secondary';
-    icon?: React.ReactNode;
-    children: React.ReactNode;
-    onClick?: () => void;
-    width?: string;
-    height?: string;
-    disabled?: boolean;
-    size?: 'big' | 'mini';
-    type?: 'button' | 'submit' | 'reset',
-    style?: React.CSSProperties;
+export interface IButtonProps {
+    className?: 'primary' | 'secondary' | 'outline-primary' | 'outline-secondary';    /** Kiểu button */
+    icon?: React.ReactNode;                                                          /** Icon trong button */
+    children: React.ReactNode;                                                      /** Nội dung button */
+    onClick?: () => void;                                                          /** Sự kiện click */
+    width?: string;                                                               /** Chiều rộng */
+    height?: string;                                                             /** Chiều cao */
+    disabled?: boolean;                                                         /** Vô hiệu hóa button */
+    size?: 'big' | 'mini';                                                     /** Kích thước button */
+    type?: 'button' | 'submit' | 'reset';                                     /** Loại button */
+    style?: React.CSSProperties;                                             /** CSS tùy chỉnh */
 }
