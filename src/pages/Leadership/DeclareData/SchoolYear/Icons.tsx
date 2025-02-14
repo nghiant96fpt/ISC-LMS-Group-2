e13cffef4dffd66e48905b01d01f4b0a36927be5
phucnh204 <137475114+phucnh204@users.noreply.github.com>
2025-02-14 18:15:26 +0700

    Thiết kế form table cơ bản với các thông tin cầ n thiết

---
+++ src/pages/Leadership/DeclareData/SchoolYear/Icons.tsx
@@ -0,0 +1,14 @@
+import pencil_icon from '../../../../../src/assets/icons/icon-fi_edit.png';
+import trash_icon from '../../../../../src/assets/icons/icon-fi_trash-2.png';
+import search_icon from '../../../../../src/assets/icons/fi_search.png';
+import arrow_up_icon from '../../../../../src/assets/icons/icon-arrow-up.png';
+import arrow_down_icon from '../../../../../src/assets/icons/icon-arrow-down.png';
+
+
+export const Icons = {
+  pencil_icon,
+  trash_icon,
+  search_icon,
+  arrow_up_icon,
+  arrow_down_icon,
+};
---
+++ src/pages/Leadership/DeclareData/SchoolYear/type.tsx
@@ -0,0 +1,7 @@
+// interface niên khóa
+export interface SchoolYearData {
+  id: number;
+  year: string;
+  start: string;
+  end: string;
+}
