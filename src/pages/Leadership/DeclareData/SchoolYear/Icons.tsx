5f7f0ed827152b10bed92ea2dcfca69f2e1cb5d5
phucnh204 <137475114+phucnh204@users.noreply.github.com>
2025-02-14 23:01:50 +0700

    Chia nhỏ folder
    - Chỉnh suaử vài chỗ theo config tailwind
    - Chia nhỏ thư mục School Year

--- src/pages/Leadership/DeclareData/SchoolYear/Icons.tsx
+++ src/pages/Leadership/DeclareData/SchoolYear/Icons.tsx
@@ -3,6 +3,8 @@ import trash_icon from '../../../../../src/assets/icons/icon-fi_trash-2.png';
 import search_icon from '../../../../../src/assets/icons/fi_search.png';
 import arrow_up_icon from '../../../../../src/assets/icons/icon-arrow-up.png';
 import arrow_down_icon from '../../../../../src/assets/icons/icon-arrow-down.png';
+import arrow_left from '../../../../../src/assets/icons/arrow-left-2.png';
+import arrow_right from '../../../../../src/assets/icons/arrow-right-2.png';
 
 
 export const Icons = {
@@ -11,4 +13,6 @@ export const Icons = {
   search_icon,
   arrow_up_icon,
   arrow_down_icon,
+  arrow_left,
+  arrow_right,
 };
--- src/pages/Leadership/DeclareData/index.tsx
+++ src/pages/Leadership/DeclareData/index.tsx
@@ -1,12 +1,13 @@
-import SchoolYear from './SchoolYear/SchoolYear';
+import SchoolYear from './SchoolYear';
 
 const DeclareData: React.FC = () => {
   return (
     <>
+       {/* <h1 className="text-3xl font-bold text-center text-black-text">Khai Báo Dữ Liệu</h1>; */}
       <SchoolYear />
     </>
   );
-  // <h1 className="text-3xl font-bold text-center text-black-text">Khai Báo Dữ Liệu</h1>;
+  
 };
 
 export default DeclareData;
