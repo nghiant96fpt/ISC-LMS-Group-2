974f841a7a81383f146020f757431b140991da06
phucnh204 <137475114+phucnh204@users.noreply.github.com>
2025-02-15 11:44:15 +0700

    Thêm url cho task niên khóa

--- src/routes/LeadershipRoutes/index.tsx
+++ src/routes/LeadershipRoutes/index.tsx
@@ -1,14 +1,15 @@
-import { Routes, Route } from "react-router-dom";
-import MainLayout from "../../layouts/MainLayout";
-import Dashboard from "../../pages/Leadership/Dashboard";
-import AllStudentProfiles from "../../pages/Leadership/AllStudentProfiles";
-import AllTeacherProfiles from "../../pages/Leadership/AllTeacherProfiles";
-import TransferAcceptance from "../../pages/Leadership/TransferAcceptance";
-import DeclareData from "../../pages/Leadership/DeclareData";
-import Exams from "../../pages/Leadership/Exams";
-import StudentRetention from "../../pages/Leadership/StudentRetention";
-import SystemSettings from "../../pages/Leadership/SystemSettings";
-import TeachingAssignment from "../../pages/Leadership/TeachingAssignment";
+import { Routes, Route } from 'react-router-dom';
+import MainLayout from '../../layouts/MainLayout';
+import Dashboard from '../../pages/Leadership/Dashboard';
+import AllStudentProfiles from '../../pages/Leadership/AllStudentProfiles';
+import AllTeacherProfiles from '../../pages/Leadership/AllTeacherProfiles';
+import TransferAcceptance from '../../pages/Leadership/TransferAcceptance';
+import DeclareData from '../../pages/Leadership/DeclareData';
+import Exams from '../../pages/Leadership/Exams';
+import StudentRetention from '../../pages/Leadership/StudentRetention';
+import SystemSettings from '../../pages/Leadership/SystemSettings';
+import TeachingAssignment from '../../pages/Leadership/TeachingAssignment';
+import SchoolYear from '../../pages/Leadership/DeclareData/SchoolYear';
 
 const StudentRoutes = () => {
   return (
@@ -19,6 +20,7 @@ const StudentRoutes = () => {
         <Route path="all-teacher-profiles" element={<AllTeacherProfiles />} />
         <Route path="transfer-acceptance" element={<TransferAcceptance />} />
         <Route path="declare-data" element={<DeclareData />} />
+        <Route path="declare-data/school-year" element={<SchoolYear />} /> {/*route cho school-year - group-4*/}
         <Route path="exams" element={<Exams />} />
         <Route path="student-retention" element={<StudentRetention />} />
         <Route path="system-settings" element={<SystemSettings />} />
