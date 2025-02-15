f27eeaf18878a12a8c9090c5d65e99557cb0673e
phucnh204 <137475114+phucnh204@users.noreply.github.com>
2025-02-15 11:50:36 +0700

    thêm url cho task school year

--- src/pages/Leadership/DeclareData/index.tsx
+++ src/pages/Leadership/DeclareData/index.tsx
@@ -1,13 +1,12 @@
-import SchoolYear from './SchoolYear';
+
 
 const DeclareData: React.FC = () => {
   return (
     <>
-       {/* <h1 className="text-3xl font-bold text-center text-black-text">Khai Báo Dữ Liệu</h1>; */}
-      <SchoolYear />
+      <h1 className="text-3xl font-bold text-center text-black-text">Khai Báo Dữ Liệu</h1>
+      {/* <SchoolYear /> */}
     </>
   );
-  
 };
 
 export default DeclareData;
