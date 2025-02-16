7eb1c64deb1c645c7139688aed86598d08d496a4
lequocan948 <zennosreal@gmail.com>
2025-02-16 14:24:08 +0700

    #101  Group2/An/list

---
+++ src/pages/Leadership/DeclareData/data.tsx
@@ -0,0 +1,7 @@
+import { SubjectGroup } from "./type";
+
+export const subjectGroups: SubjectGroup[] = [
+    { name: "Văn hóa xã hội", head: "Nguyễn Văn A" },
+    { name: "Khoa học tự nhiên", head: "Trần Thị B" },
+    { name: "Anh Văn", head: "Phạm Ngọc C" },
+];
