b1867ef5b93dd80accb77d98177e01a559faa4d5
thuysvy <150228593+thuysvy@users.noreply.github.com>
2025-02-16 22:29:49 +0700

    Giao diện thiết lập Khoa-Khối

--- src/pages/Leadership/DeclareData/BlockDepartment/type.tsx
+++ src/pages/Leadership/DeclareData/BlockDepartment/type.tsx
@@ -1,4 +1,13 @@
 export interface IDepartment {
-    id: string;
-    name: string;
+  id: string;
+  name: string;
+}
+export interface ModalProps {
+  title: string; // Tiêu đề của modal
+  code: string; // Mã khoa - khối
+  faculty: string; // Tên khoa - khối
+  headOfFaculty: string; // Trưởng khoa - khối đang được chọn
+  facultyOptions: string[]; // Danh sách trưởng khoa - khối để hiển thị trong select
+  onClose: () => void; // Hàm xử lý khi nhấn nút "Hủy"
+  onSave: () => void; // Hàm xử lý khi nhấn nút "Lưu"
 }
