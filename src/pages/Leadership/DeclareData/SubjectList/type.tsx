1ee5a3b40a2554be55f9b760401becaa9bf4bb76
tranhuudat050803 <132972331+tranhuudat050803@users.noreply.github.com>
2025-02-17 15:56:28 +0700

    fix dsbomon

---
+++ src/pages/Leadership/DeclareData/SubjectList/type.tsx
@@ -0,0 +1,16 @@
+export interface Subject {
+  id: string;
+  name: string;
+}
+
+export const subjects: Subject[] = [
+  { id: 'vl009', name: 'Vật Lý 9' },
+  { id: 'hh010', name: 'Hoá Học 10' },
+  { id: 'sh010', name: 'Sinh Học 10' },
+  { id: 'vl010', name: 'Vật Lý 10' },
+  { id: 'vl009', name: 'Vật Lý 9' },
+  { id: 'hh010', name: 'Hoá Học 10' },
+  { id: 'vl010', name: 'Vật Lý 10' },
+  { id: 'vl009', name: 'Vật Lý 9' },
+  { id: 'hh010', name: 'Hoá Học 10' },
+];
