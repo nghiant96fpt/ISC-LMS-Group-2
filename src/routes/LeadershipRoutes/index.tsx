7385ebcf1540d99311edb2fa61439b72b1eddea7
Nguyen Trong Nghia (FPL CT) <nghiant96@fpt.edu.vn>
2025-02-20 14:10:25 +0700

    fix crash merge code

--- src/routes/LeadershipRoutes/index.tsx
+++ src/routes/LeadershipRoutes/index.tsx
@@ -10,9 +10,9 @@ import StudentRetention from '../../pages/Leadership/StudentRetention';
 import SystemSettings from '../../pages/Leadership/SystemSettings';
 import TeachingAssignment from '../../pages/Leadership/TeachingAssignment';
 import SchoolYearAdd from '../../pages/Leadership/DeclareData/SchoolYear/SchoolYearAdd';
-import SchoolYearEditPages from "../../pages/Leadership/DeclareData/SchoolYear/SchoolYearEditPages";
+import SchoolYearEditPages from '../../pages/Leadership/DeclareData/SchoolYear/SchoolYearEditPages';
 import SchoolYear from '../../pages/Leadership/DeclareData/SchoolYear/SchoolYearTable';
-import DeclareDataRoutes from "../../pages/Leadership/DeclareData/DeclareDataRoutes";
+import DeclareDataRoutes from '../../pages/Leadership/DeclareData/DeclareDataRoutes';
 import DepartmentSettings from '../../pages/Leadership/DeclareData/SetupDepartmentModal';
 import SubjectList from '../../pages/Leadership/DeclareData/SubjectList';
 
@@ -24,15 +24,13 @@ const StudentRoutes = () => {
         <Route path="all-student-profiles" element={<AllStudentProfiles />} />
         <Route path="all-teacher-profiles" element={<AllTeacherProfiles />} />
         <Route path="transfer-acceptance" element={<TransferAcceptance />} />
-
         {/*route cho school-year - group-4*/}
         <Route path="declare-data/school-year" element={<SchoolYear />} /> {/* url table niên khóa */}
         <Route path="declare-data/school-year/edit-school-year" element={<SchoolYearEditPages />} /> {/* url sửa niên khóa */}
         <Route path="declare-data/school-year/add-school-year" element={<SchoolYearAdd />} /> {/* url thêm niên khóa */}
         {/*  */}
-        
         <Route path="declare-data/*" element={<DeclareDataRoutes />} />
-<!--         <Route path="declare-data" element={<DeclareData />} /> -->
+        {/* <Route path="declare-data" element={<DeclareData />} /> */}
         <Route path="declare-data/edit" element={<DepartmentSettings />} />
         <Route path="declare-data/subject-list" element={<SubjectList />} />
         <Route path="exams" element={<Exams />} />
