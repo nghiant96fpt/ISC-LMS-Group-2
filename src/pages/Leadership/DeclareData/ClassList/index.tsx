ac3257f547fcefefdb2a1cb5cac70f596348abb6
phucnh204 <137475114+phucnh204@users.noreply.github.com>
2025-02-21 21:37:07 +0700

    Tao folder Class List suwr dungj cho task DS Lop hocj

---
+++ src/pages/Leadership/DeclareData/ClassList/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const ClassList = () => {
+    return (
+        <div>
+            {/* Component Header Class List */}
+            <div></div>
+
+{/* Component Class List table */}
+
+{/* Component Phân trang */}
+        </div>
+    );
+}
+
+export default ClassList;
