f2c4a09471a9c85cce155dd81ca6d339db602f1b
doandvpc06922 <150895608+doandvpc06922@users.noreply.github.com>
2025-02-23 22:13:50 +0700

    Update index.tsx

--- src/routes/LeadershipRoutes/index.tsx
+++ src/routes/LeadershipRoutes/index.tsx
@@ -9,9 +9,14 @@ import Exams from '../../pages/Leadership/Exams';
 import StudentRetention from '../../pages/Leadership/StudentRetention';
 import SystemSettings from '../../pages/Leadership/SystemSettings';
 import TeachingAssignment from '../../pages/Leadership/TeachingAssignment';
-import SchoolYear from '../../pages/Leadership/DeclareData/SchoolYear';
+// import SchoolYear from '../../pages/Leadership/DeclareData/SchoolYear';
 import Uploadfile from '../../pages/Leadership/Uploadfile';
-
+// import SchoolYearAdd from '../../pages/Leadership/DeclareData/SchoolYear/SchoolYearAdd';
+// import SchoolYearEditPages from '../../pages/Leadership/DeclareData/SchoolYear/SchoolYearEditPages';
+// import SchoolYear from '../../pages/Leadership/DeclareData/SchoolYear/SchoolYearTable';
+// import DeclareDataRoutes from '../../pages/Leadership/DeclareData/DeclareDataRoutes';
+// import DepartmentSettings from '../../pages/Leadership/DeclareData/SetupDepartmentModal';
+// import SubjectList from '../../pages/Leadership/DeclareData/SubjectList';
 const StudentRoutes = () => {
   return (
     <Routes>
@@ -22,7 +27,15 @@ const StudentRoutes = () => {
         <Route path="transfer-acceptance" element={<TransferAcceptance />} />
         <Route path="uploadfile" element={<Uploadfile />} />
         <Route path="declare-data" element={<DeclareData />} />
-        <Route path="declare-data/school-year" element={<SchoolYear />} /> {/*route cho school-year - group-4*/}
+        {/* <Route path="declare-data/school-year" element={<SchoolYear />} /> route cho school-year - group-4 */}
+        {/* <Route path="declare-data/school-year" element={<SchoolYear />} /> url table niên khóa */}
+        {/* <Route path="declare-data/school-year/edit-school-year" element={<SchoolYearEditPages />} /> url sửa niên khóa */}
+        {/* <Route path="declare-data/school-year/add-school-year" element={<SchoolYearAdd />} /> url thêm niên khóa */}
+        {/*  */}
+        {/* <Route path="declare-data/*" element={<DeclareDataRoutes />} /> */}
+        {/* <Route path="declare-data" element={<DeclareData />} /> */}
+        {/* <Route path="declare-data/edit" element={<DepartmentSettings />} /> */}
+        {/* <Route path="declare-data/subject-list" element={<SubjectList />} /> */}
         <Route path="exams" element={<Exams />} />
         <Route path="student-retention" element={<StudentRetention />} />
         <Route path="system-settings" element={<SystemSettings />} />
