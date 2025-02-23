70a41efe8e20a2b59f3258d41a458416f1ad093d
AnTruqhy <tholhpc06297@fpt.edu.vn>
2025-02-24 00:28:04 +0700

    Thêm props ref vào cho component Input

--- src/components/Input/type.tsx
+++ src/components/Input/type.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 export interface inputProps {
-  outline?: 'primary-outline', 
-  disabled?: boolean, 
-  placeholder?: string,
-  icon?: React.ReactNode,
-  size?: 'sm' | 'md' | 'lg',
-  style?: React.CSSProperties,
-  className?: string,
-  type?: 'text' | 'password',
+  outline?: 'primary-outline';
+  disabled?: boolean;
+  placeholder?: string;
+  icon?: React.ReactNode;
+  size?: 'sm' | 'md' | 'lg';
+  style?: React.CSSProperties;
+  className?: string;
+  type?: 'text' | 'password';
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  ref: React.RefObject<any>;
 };
-
-// cách sử dụng ref tránh lỗi nhất (theo chatGPT): const aRef = useRef<HTMLInputElement>(null);
