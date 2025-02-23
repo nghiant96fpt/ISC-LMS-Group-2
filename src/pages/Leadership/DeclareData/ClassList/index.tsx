41d4fffc62f2307db67da8248b23724e3221278f
phucnh204 <137475114+phucnh204@users.noreply.github.com>
2025-02-23 23:11:39 +0700

    Update index.tsx

--- src/pages/Leadership/DeclareData/ClassList/index.tsx
+++ src/pages/Leadership/DeclareData/ClassList/index.tsx
@@ -3,12 +3,7 @@ import React from 'react';
 const ClassList = () => {
   return (
     <div>
-      {/* Component Header Class List */}
-      <div></div>
-
-      {/* Component Class List table */}
-
-      {/* Component Phân trang */}
+      
     </div>
   );
 };
