8e1b8433899cba740345690148ee5cba752d2e76
TuiLaHuy <156669821+TuiLaHuy@users.noreply.github.com>
2025-02-24 12:01:05 +0700

    commit đổi tên file

--- src/pages/Leadership/DeclareData/ScoreTypes/type.tsx
+++ src/pages/Leadership/DeclareData/ScoreTypes/type.tsx
@@ -1,8 +1,15 @@
 export interface IScoreType {
-    id: number;
-    name: string;
-    coefficient: number;
-    semester1: number;
-    semester2: number;
-  }
-  
+  id: number;
+  name: string;
+  coefficient: number;
+  semester1: number;
+  semester2: number;
+}
+
+/* Type */
+export interface GradeTypeFormData {
+  gradeTypeName: string;
+  coefficient: string;
+  semester1: string;
+  semester2: string;
+}
