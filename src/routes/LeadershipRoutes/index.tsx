0d86167ef28ebd3c9db50ae50b13cdcbbd62845a
doandvpc06922 <150895608+doandvpc06922@users.noreply.github.com>
2025-02-24 11:02:58 +0700

    .

--- src/routes/LeadershipRoutes/index.tsx
+++ src/routes/LeadershipRoutes/index.tsx
@@ -17,6 +17,7 @@ import DepartmentSettings from '../../pages/Leadership/DeclareData/SetupDepartme
 import SubjectList from '../../pages/Leadership/DeclareData/SubjectList';
 import NewClass from '../../pages/Leadership/DeclareData/ClassList/NewClass';
 import UpdateClass from '../../pages/Leadership/DeclareData/ClassList/UpdateClass';
+import Uploadfile from '../../pages/Leadership/DeclareData/ClassList/Uploadfile';
 
 const StudentRoutes = () => {
   return (
@@ -33,6 +34,7 @@ const StudentRoutes = () => {
         {/* route cho class-list - group-4 */}
         <Route path="declare-data/add-class" element={<NewClass />} />
         <Route path="declare-data/update-class" element={<UpdateClass />} />
+        <Route path="declare-data/file-class" element={<Uploadfile />} />
         <Route path="declare-data/*" element={<DeclareDataRoutes />} />
         {/* <Route path="declare-data" element={<DeclareData />} /> */}
         <Route path="declare-data/edit" element={<DepartmentSettings />} />
