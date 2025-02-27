cd00b6b8d79ad22e5e3ee1c8ccb52f24d9be3d70
tranvanchilinh1003 <chilinh140220@gmail.com>
2025-02-27 17:49:30 +0700

    Grop-3_Linh_LecturerProfile

---
+++ src/pages/Leadership/AllTeacherProfiles/type.tsx
@@ -0,0 +1,9 @@
+export interface ILecturerProfile {
+    id: string;
+    name: string;
+    dob: string;
+    gender: string;
+    department: string;
+    position: string;
+    status: string;
+}
