93f5608d09b105334ab1bfa58c30f56ea7a3b263
phucnh204 <137475114+phucnh204@users.noreply.github.com>
2025-03-04 21:56:48 +0700

    Chia layoút cho dashboard-teacher

--- src/pages/Teacher/TeacherDashboard/index.jsx
+++ src/pages/Teacher/TeacherDashboard/index.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const TeacherDashboard = () => {
-  return <div className="text-3xl font-bold text-center text-black-text">Tổng quan</div>;
+  return (
+    <div className="p-6 bg-gray-100 h-full">
+      {/* Tiêu đề chính */}
+      <div className="flex gap-6 mb-6">
+        <div className="lg:w-[40%] bg-red-100">
+          <h1 className="text-3xl font-bold text-gray-800">Tổng quan</h1>
+          {/* component tổng quan */}
+        </div>
+        '
+        <div className="lg:w-[60%] bg-blue-100">
+          <h1 className="text-3xl font-bold text-gray-800 text-start">Tất cả khóa học</h1>
+          {/* component table */}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TeacherDashboard;
