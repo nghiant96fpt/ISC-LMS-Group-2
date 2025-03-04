deb0f8920447a6c6c8034d8df6d5c8da5c490120
phucnh204 <137475114+phucnh204@users.noreply.github.com>
2025-03-04 22:12:00 +0700

    Update index.jsx

--- src/pages/Teacher/TeacherDashboard/index.jsx
+++ src/pages/Teacher/TeacherDashboard/index.jsx
@@ -9,7 +9,7 @@ const TeacherDashboard = () => {
           <h1 className="text-3xl font-bold text-gray-800">Tổng quan</h1>
           {/* component tổng quan */}
         </div>
-        '
+        
         <div className="lg:w-[60%] bg-blue-100">
           <h1 className="text-3xl font-bold text-gray-800 text-start">Tất cả khóa học</h1>
           {/* component table */}
