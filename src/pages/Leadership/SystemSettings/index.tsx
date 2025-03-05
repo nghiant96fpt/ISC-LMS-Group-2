79ce3179a0416d251cf0d25efebde5df0633074d
AnTruqhy <tholhpc06297@fpt.edu.vn>
2025-03-06 03:52:03 +0700

    Leadership-setting-config take-1-060325

--- src/pages/Leadership/SystemSettings/index.tsx
+++ src/pages/Leadership/SystemSettings/index.tsx
@@ -1,5 +1,90 @@
+import React from 'react';
+import { useNavigate } from 'react-router';
+
 const SystemSettings: React.FC = () => {
-    return <h1 className="text-3xl font-bold text-center text-black-text">Cài đặt hệ thống</h1>;
+  const navigator = useNavigate();
+  const buttons = [
+    {
+      variant: 'orange',
+      title: (
+        <div className="text-center">
+          <h1 className="text-white text-[28px] font-bold">Cấu hình</h1>
+          <p className="text-white font-bold">Theme, các thông tin khác về cấu hình</p>
+        </div>
+      ),
+      handle: () => {
+        navigator('/leadership/system-settings/config');
+      }
+    },
+    {
+      variant: 'orange',
+      title: (
+        <div className="text-center">
+          <h1 className="text-white text-[28px] font-bold">Thông tin trường</h1>
+          <p className="text-white font-bold">Thông tin chung của trường, các cơ sở</p>
+        </div>
+      ),
+    },
+    {
+      variant: 'orange',
+      title: (
+        <div className="text-center">
+          <h1 className="text-white text-[28px] font-bold">Người dùng hệ thống</h1>
+          <p className="text-white font-bold px-2">Phần nhóm người dùng, quản lý thông tin người dùng và phân quyền sử dụng</p>
+        </div>
+      ),
+    },
+    {
+      variant: 'primary',
+      title: (
+        <div className="text-center">
+          <h1 className="text-white text-[28px] font-bold">Thiết lập lớp học</h1>
+          <p className="text-white font-bold">Loại lớp cơ bản, nâng cao</p>
+        </div>
+      ),
+    },
+    {
+      variant: 'primary',
+      title: (
+        <div className="text-center">
+          <h1 className="text-white text-[28px] font-bold">Thiết lập môn học</h1>
+          <p className="text-white font-bold">Thông tin các hệ đào tạo của trường</p>
+        </div>
+      ),
+      handle: () => {
+        navigator('/leadership/system-settings/subject-management');
+      }
+    },
+    {
+      variant: 'primary',
+      title: (
+        <div className="text-center">
+          <h1 className="text-white text-[28px] font-bold px-2">Quản lý trình độ đào tạo</h1>
+          <p className="text-white font-bold">Thông tin các hệ đào tạo của trường</p>
+        </div>
+      ),
+    },
+  ];
+  return (
+    <div className="pr-20 pl-10">
+      <div className="w-[100%]">
+        <p className="text-3xl font-bold mb-5">Cài đặt hệ thống</p>
+        <div className="w-full flex justify-around flex-wrap">
+          {buttons.map((item, index) => (
+            <div
+              key={index}
+              className={`w-[340px] h-[120px] mb-4 ${
+                item?.variant === 'orange' ? 'bg-gradient-to-r from-[#F17F21] to-[#FF5400]' : 'bg-gradient-to-r from-[#56CCF2] to-[#2F80ED]'
+              } flex items-center justify-center rounded-xl`}
+              onClick={item?.handle}
+            >
+              {item?.title}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
-  
+
 export default SystemSettings;
