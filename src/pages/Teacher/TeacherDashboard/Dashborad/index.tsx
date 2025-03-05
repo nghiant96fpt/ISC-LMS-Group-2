a170c1e1ae4ebdd0b5cd7a20a8a6b8584b0d21c2
phucnh204 <137475114+phucnh204@users.noreply.github.com>
2025-03-05 14:39:42 +0700

    Cập nhật task dasboard-teacher

--- src/pages/Teacher/TeacherDashboard/Dashborad/index.tsx
+++ src/pages/Teacher/TeacherDashboard/Dashborad/index.tsx
@@ -1,74 +1,55 @@
 import React from 'react';
-import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, PieChart, Pie, Cell } from 'recharts';
 
-const data = [
-  { name: 'Giỏi', value: 10 },
-  { name: 'Khá', value: 125 },
-  { name: 'Trung bình', value: 75 },
-  { name: 'Yếu', value: 0 },
-];
-
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+import './index.css';
+import Chart from './Chart/Chart';
 
 const Dashboard = () => {
   return (
-    <div>
-      <div className="bg-white font-bold w-full rounded-xl shadow-md p-4">
-        <div className="grid grid-cols-2 gap-4">
-          <div className="p-4 border rounded-lg">
-            <h3 className="text-lg font-semibold">Khóa học của tôi</h3>
-            <p className="text-2xl text-blue-500">10</p>
+    <div className="">
+      <div className=" font-bold w-full rounded-xl  p-1 my-2">
+        <div className="grid grid-cols-2 gap-2">
+          <div className="custom-css-gird  from-background-1 to-background-2">
+            <h3 className="custom-text-title">Khóa học của tôi</h3>
+            <p className="custom-text-data ">10</p>
           </div>
-          <div className="p-4 border rounded-lg">
-            <h3 className="text-lg font-semibold">Lớp học Online</h3>
-            <p className="text-2xl text-green-500">2</p>
+          <div className="custom-css-gird  from-background-6 to-background-blue-2">
+            <h3 className="custom-text-title">Lớp học Online</h3>
+            <p className=" custom-text-data ">2</p>
           </div>
-          <div className="p-4 border rounded-lg">
-            <h3 className="text-lg font-semibold">Bài kiểm tra chưa chấm</h3>
-            <p className="text-2xl text-yellow-500">8</p>
+          <div className="custom-css-gird  from-background-4 to-background-3">
+            <h3 className="custom-text-title">Bài kiểm tra chưa chấm</h3>
+            <p className=" custom-text-data ">8</p>
           </div>
-          <div className="p-4 border rounded-lg">
-            <h3 className="text-lg font-semibold">Hỏi đáp Q & A</h3>
-            <p className="text-2xl text-red-500">5</p>
+          <div className="custom-css-gird  from-background-6 to-background-5">
+            <h3 className="custom-text-title">Hỏi đáp Q & A</h3>
+            <p className=" custom-text-data ">5</p>
           </div>
         </div>
       </div>
 
-      <h2 className="text-xl">Thống kê kết quả học tập của học viên</h2>
-      <div className="bg-white font-bold w-full rounded-xl shadow-md p-4">
-        <div className="flex h-56">
-          <div className="w-[50%]">
-            <PieChart width={225} height={200}>
-              <Pie
-                data={data}
-                cx="50%"
-                cy="50%"
-                labelLine={false}
-                label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
-                outerRadius={80}
-                fill="#8884d8"
-                dataKey="value"
-              >
-                {data.map((entry, index) => (
-                  <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                ))}
-              </Pie>
-              <Tooltip />
-            </PieChart>
+      <h2 className="shadow-boxShadow text-xl font-semibold mb-0.5">Thống kê kết quả học tập của học viên</h2>
+      <div className="bg-white  w-full rounded-xl shadow-md p-4">
+        {/* Component chart */}
+        <Chart />
+
+        {/* Chú thích */}
+        <div className="flex flex-col gap-3">
+          {/*  */}
+          <div className="flex items-center gap-1">
+            <div className="w-6 h-4 rounded-md bg-gradient-to-r from-blue-500 to-blue-400"></div>
+            <span className="text-gray-500 text-xs">Tổng số học sinh giỏi</span>
           </div>
-          <div className="w-[50%] flex flex-col justify-center pl-4">
-            <div className="text-lg">
-              Tổng số học sinh giỏi: <span className="text-blue-500">300</span>
-            </div>
-            <div className="text-lg">
-              Tổng số học sinh khá: <span className="text-green-500">125</span>
-            </div>
-            <div className="text-lg">
-              Tổng số học sinh trung bình: <span className="text-yellow-500">75</span>
-            </div>
-            <div className="text-lg">
-              Tổng số học sinh yếu: <span className="text-red-500">0</span>
-            </div>
+
+          {/*  */}
+          <div className="flex items-center gap-1">
+            <div className="w-6 h-4 rounded-md bg-gradient-to-r from-yellow-500 to-yellow-300"></div>
+            <span className="text-gray-500 text-xs">Tổng số học sinh khá</span>
+          </div>
+
+          {/* */}
+          <div className="flex items-center gap-1">
+            <div className="w-6 h-4 rounded-md bg-gradient-to-r from-green-500 to-green-300"></div>
+            <span className="text-gray-500 text-xs">Tổng số học sinh trung bình</span>
           </div>
         </div>
       </div>
--- src/pages/Teacher/TeacherDashboard/index.tsx
+++ src/pages/Teacher/TeacherDashboard/index.tsx
@@ -3,16 +3,16 @@ import Dashboard from './Dashborad';
 
 const TeacherDashboard = () => {
   return (
-    <div className="p-6 bg-gray-100 h-full">
+    <div className="  h-full">
       {/* Tiêu đề chính */}
-      <div className="flex gap-6 mb-6">
-        <div className="lg:w-[40%] bg-red-100 p-4">
+      <div className="flex gap-2 mb-6">
+        <div className="lg:w-[40%]">
           <h1 className="text-3xl font-bold text-gray-800">Tổng quan</h1>
-          {/* component tổng quan */}
+          {/* component tổng quan teacher*/}
           <Dashboard/>
         </div>
         
-        <div className="lg:w-[60%] bg-blue-100">
+        <div className="lg:w-[60%]">
           <h1 className="text-3xl font-bold text-gray-800 text-start">Tất cả khóa học</h1>
           {/* component table */}
         </div>
