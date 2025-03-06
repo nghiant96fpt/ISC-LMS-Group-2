b6a38b8370b4278742c6e5deb9d23dd55d1a139b
tranvanchilinh1003 <chilinh140220@gmail.com>
2025-03-06 22:16:38 +0700

    update new

--- src/pages/Teacher/ClassList/ClassDetail/ClassDeatail.tsx
+++ src/pages/Teacher/ClassList/ClassDetail/ClassDeatail.tsx
@@ -221,7 +221,7 @@ const ClassDetail: React.FC = () => {
                                             {isUpcoming && (
                                                 <div className="mt-2 flex justify-center gap-2">
                                                     <button className={`w-8 h-8 flex items-center justify-center text-gray-300`} onClick={handleEdit.bind(null, session)}>
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="3232" fill="currentColor" className={`bi bi-pencil-square ${editSession?.id == session.id ? 'text-gray-400' : 'text-orange-500'}`} viewBox="0 0 16 16">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className={`bi bi-pencil-square ${editSession?.id == session.id ? 'text-gray-400' : 'text-orange-500'}`} viewBox="0 0 16 16">
                                                             <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
                                                             <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
                                                         </svg>
