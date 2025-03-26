2da30cd67b46c03e0b65c166e8fc0c788cd62fcf
nmphongk2 <nm.phong2002@gmail.com>
2025-03-26 16:52:13 +0700

    Api_KBDL_To-BoMOn-List-Xoa

--- src/services/Leadership/DeclareData/Department/teacherService.tsx
+++ src/services/Leadership/DeclareData/Department/teacherService.tsx
@@ -1,27 +1,19 @@
-import axios from 'axios';
-
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://fivefood.shop/api/';
+import createAxiosInstance from '../../../../utils/axiosInstance';
+const axiosInstance = createAxiosInstance(true);
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 if (!API_BASE_URL) {
   console.error('LỖI: API_BASE_URL không được thiết lập trong .env!');
 }
 
-export const fetchTeacherListAPI = async (page = 1, pageSize = 10, sortColumn = 'Id', sortOrder = 'asc') => {
-  const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://fivefood.shop/api';
-
-  const token = localStorage.getItem('token');
-
-  const response = await axios.get(`${API_BASE_URL}/teacherlists`, {
-    params: { page, pageSize, sortColumn, sortOrder },
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
-    },
+export const fetchTeacherListAPI = async (page = 1, pageSize = 10, sortColumn = 'Id', sortOrder = 'asc', search = '') => {
+  const response = await axiosInstance.get(`${API_BASE_URL}/teacherlists`, {
+    params: { page, pageSize, sortColumn, sortOrder, search },
   });
 
-  return response.data.data;
+  return response.data;
 };
 export const deleteTeacherAPI = async (id: number) => {
-  const response = await axios.delete(`${API_BASE_URL}teaching-assignments/${id}`);
+  const response = await axiosInstance.delete(`${API_BASE_URL}/teaching-assignments/${id}`);
   return response.data;
 };
